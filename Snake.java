b795b5c123f203030830f858f204374724ecb11c
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-22 12:48:28 -0400

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,1 +1,41 @@
+package SnakeGame;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+//This is the main class.//
+
+public class Snake extends JFrame {
+  
+  public Snake() {
+    
+    initUI();
+	}
+	
+	private void initUI() {
+		
+		add(new Board());
+		
+		setResizable(false);
+		pack();
+		
+		//The setRezisable() method affects the insets of the JFrame container on some platforms//
+		//Therefore, it is important to call it before the pack() method//
+		//Otherwise, the collision of the snake with the right and bottom borders might not work correctly.//
+		
+		setTitle("Snake");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+			JFrame ex = new Snake();
+			ex.setVisible(true);
+		});
+	}
+	
+	
+	
+  
